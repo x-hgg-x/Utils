c290e779fc211ec83e719ac76c2c7ad3c7dfe3ff
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2018-10-06 21:15:48 +0200

    Update nemo_save.sh

--- Nemo save and restore session/nemo_save.sh
+++ Nemo save and restore session/nemo_save.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 declare -x DISPLAY=":0.0"
-wmctrl -lx | grep nemo.Nemo | sed -e 's/Dossier\ personnel/ - ./; s/Corbeille/ - trash:/' | awk -F' - ' '{print $2}' > ~/nemo.dat
+wmctrl -lx | grep nemo.Nemo | sed -e "s#Dossier\ personnel# - $HOME#; s#Corbeille# - trash:#" | awk -F' - /' '{print "/"$2}' > ~/nemo.dat
